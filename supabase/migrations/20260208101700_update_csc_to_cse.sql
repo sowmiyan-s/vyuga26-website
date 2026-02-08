dac4e9e608d529a15de36e2dd9d77619fa8f865c
sowmian_s <sowmisowmiyan58@gmail.com>
2026-02-08 20:27:18 +0530

    sql

--- supabase/migrations/20260208101700_update_csc_to_cse.sql
+++ supabase/migrations/20260208101700_update_csc_to_cse.sql
@@ -1,14 +1,14 @@
 -- Migration to update CSC to CSE in intercollege_registrations
 
--- 1. Update existing data
-UPDATE public.intercollege_registrations 
-SET department = 'CSE' 
-WHERE department = 'CSC';
-
--- 2. Drop the existing check constraint
+-- 1. Drop the existing check constraint FIRST to allow the update
 -- We use IF EXISTS to avoid errors if the constraint name is different or doesn't exist
 ALTER TABLE public.intercollege_registrations 
 DROP CONSTRAINT IF EXISTS intercollege_registrations_department_check;
+
+-- 2. Update existing data
+UPDATE public.intercollege_registrations 
+SET department = 'CSE' 
+WHERE department = 'CSC';
 
 -- 3. Add the new check constraint with CSE instead of CSC
 ALTER TABLE public.intercollege_registrations 
